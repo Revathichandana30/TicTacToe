c4320f5da65ba68f72cf619f816ecba6f21c4cc2
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-16 07:29:59 +0530

    [REVATHI]add : to check who starts the game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 echo "============TIE TAC TOE ==============="
 boardElements=( 1 2 3 4 5 6 7 8 9 )
+
 displayBoard(){
 	element=0;
 	while [ $element -lt 9 ]
@@ -8,4 +9,12 @@ displayBoard(){
 		echo " ${boardElements[$((element++))]} ${boardElements[$((element++))]} ${boardElements[$((element++))]}"
 	done
 }
+
+selectPlayer(){
+	case $((RANDOM%2)) in
+		0) echo "player starts game";;
+		1) echo "computer starts game";;
+	esac
+}
 displayBoard
+selectPlayer
