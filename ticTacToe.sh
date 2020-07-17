2d935715afa44dc6ca9e3c8d2c9abb3fe773656f
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-17 20:45:41 +0530

    [REVATHI]refactor: function statements

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
 echo "============TIE TAC TOE ==============="
 boardElements=( 1 2 3 4 5 6 7 8 9 )
-resetBoard(){
+function resetBoard(){
    for((row=1;row<=3;row++))
    do
       for((column=1;column<=3;column++))
       do
-         boardElements[$row$column]=s;
-         printf "${boardElements[$row$column]} "
+         boardElements[$row$column]="-";
+         printf "| ${boardElements[$row$column]} |"
       done
-      echo " "
+      echo -e "\n------------ "
    done
 }
 
 selectPlayer(){
 	case $((RANDOM%2)) in
-		0) echo "player starts game";;
-		1) echo "computer starts game";;
+		0) echo "player starts game";
+			startPerson=player;;
+		1) echo "computer starts game";
+			startPerson=computer;;
 	esac
 }
 resetBoard
