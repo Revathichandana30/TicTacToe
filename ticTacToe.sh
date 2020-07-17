6e62f79cd9b290089f5843dc1935afd624333c95
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-17 21:11:24 +0530

    [REVATHI]refactor: function changes has done

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,43 +1,44 @@
 #! /bin/bash
 echo "============TIE TAC TOE ==============="
-boardElements=( 1 2 3 4 5 6 7 8 9 )
+declare -a boardElements
 
-resetBoard(){
-   for((row=1;row<=3;row++))
-   do
-      for((column=1;column<=3;column++))
-      do
-         boardElements[$row$column]=s;
-         printf "${boardElements[$row$column]} "
-      done
-      echo " "
+function resetBoard(){
+	for((row=1;row<=3;row++))
+	do
+		for((column=1;column<=3;column++))
+		do
+         boardElements[$row$column]="-";
+		done
    done
 }
 
-selectPlayer(){
+function selectPlayer(){
 	case $((RANDOM%2)) in
-		0) playerLetter=`selectLetter`;
-			computerLetter=`assignLetters $playerLetter`
-			echo "player starts game and he selected $playerLetter letter and computer letter: $computerLetter ";;
-		1) computerLetter=`selectLetter`;
-			playerLetter=`assignLetters $computerLetter`
-			echo "computer starts game and it selects $computerLetter letter and player letter: $playerLetter";;
+		0) echo "persons starts the gamme";
+			startPerson=player;;
+		1) echo "computer Starts the game";
+			startPerson=computer;;
 	esac
 }
-assignLetters(){
-	if [[ $1 =~ [O] ]]
+
+function assignLetters(){
+	playerLetter=O;computerLetter=O
+	selectLetter
+	if [[ $letter =~ $playerLetter  ]]
 	then
-		temp=X
+		computerLetter=X;
 	else
-		temp=O
+		playerLetter=X;
 	fi
-	echo "$temp"
+	echo "player letter : $playerLetter and computer letter : $computerLetter"
 }
-selectLetter(){
+
+function selectLetter(){
 	case $((RANDOM%2)) in
-		0) echo "O";;
-		1) echo "X";;
+		0) letter=O;;
+		1) letter=X;;
 	esac
 }
 resetBoard
 selectPlayer
+assignLetters
